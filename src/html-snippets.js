bcf8e10c662d643a276e3d8fbbb7e1460436ac06
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-07-20 20:52:48 +0200

    initial commit: setting up

---
+++ src/html-snippets.js
@@ -0,0 +1,21 @@
+"use strict";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function header() {
+    return ` <div class="titleName">'Learn WebGPU Fundamentals' Tutorial Code </div>
+  <div class="subtitle">Follow along the <a href="https://webgpufundamentals.org/">WebGPU Fundamentals</a> articles</div>`;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function guiContainer() {
+    return '<div id="guiContainer"></div>';
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function navigationButtons() {
+    let baseURL = '';
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        baseURL = ''; // Set the base URL to empty string for localhost
+    }
+    else {
+        baseURL = 'https://www.craftlinks.art';
+    }
+    return `<a href = "${baseURL}/pages/fundamentals.html" class="button">Fundamentals</a>`;
+}
